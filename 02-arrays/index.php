5c1e39d926a527b3240537e0d4829ac6ec37528e
Lucas Delobelle <lucas@ld-web.net>
2024-03-12 09:48:58 +0100

    :sparkles: Tableaux suite
    Associatif, multidimensionnel, début exo boucles

--- 02-arrays/index.php
+++ 02-arrays/index.php
@@ -19,3 +19,32 @@ var_dump($notes);
 
 $notes[0] = 9;
 var_dump($notes);
+
+// Tableau associatif
+$customIndexes = [3 => 15, 12, 6 => 10, 65, 54, "bonbon"];
+var_dump($customIndexes);
+// echo $customIndexes[0]; // Erreur, index non défini
+$customIndexes[5] = "Nabil";
+var_dump($customIndexes);
+
+// Autre tableau associatif
+$user = [
+    'name' => 'Monica',
+    'city' => 'Lyon'
+];
+
+echo $user['city'] . '<br />';
+
+// Tableau multidimensionnel
+$users = [
+    [
+        'name' => 'Lennon',
+        'firstname' => 'John'
+    ],
+    [
+        'name' => 'Dylan',
+        'firstname' => 'Bob'
+    ]
+];
+
+echo $users[0]['firstname'];
---
+++ 02-arrays/loops.php
@@ -0,0 +1,16 @@
+<?php
+
+$notes = [1, 16, 7, 18, 6, 12, 14, 20];
+
+$users = [
+    [
+        'name' => 'Lennon',
+        'firstname' => 'John'
+    ],
+    [
+        'name' => 'Dylan',
+        'firstname' => 'Bob'
+    ]
+];
+
+// Afficher les notes et les users avec 4 types de boucles différents

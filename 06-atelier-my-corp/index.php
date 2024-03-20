e00fbc8ab3107458a8d2fc11ed7cd9856106ef44
Lucas Delobelle <lucas@ld-web.net>
2024-03-20 15:06:40 +0100

    :sparkles: Newsletter form
    index.php

--- 06-atelier-my-corp/index.php
+++ 06-atelier-my-corp/index.php
@@ -53,6 +53,23 @@ require_once 'layout/header.php';
             <?php } ?>
         </div>
     </section>
+
+    <section class="group max-w-[900px] mx-auto mt-36 bg-gradient-to-r from-blue-700 via-purple-500 to-pink-500 rounded-lg shadow-xl p-12 text-white relative">
+        <div class="absolute right-14 top-10 translate-y-2 opacity-100 group-hover:opacity-0 group-hover:translate-y-0 transition-all duration-200">
+            <?php require_once 'templates/mail_closed.php'; ?>
+        </div>
+        <div class="absolute right-14 top-10 translate-y-2 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-200">
+            <?php require_once 'templates/mail_opened.php'; ?>
+        </div>
+        <h2 class="text-5xl font-sans mb-6">Inscrivez-vous à la newsletter !</h2>
+        <form method="GET" class="flex gap-x-6">
+            <div class="grow flex items-center">
+                <label for="email" class="text-xl mr-6">Email :</label>
+                <input type="email" id="email" class="!text-black grow focus:outline-none" placeholder="email@test.com" />
+            </div>
+            <div><button type="submit" class="bg-orange-400 rounded-lg uppercase py-2 px-4 hover:bg-orange-500 transition-all shadow-lg">Inscription</button></div>
+        </form>
+    </section>
 </main>
 
 <?php require_once 'layout/footer.php';
--- 06-atelier-my-corp/layout/footer.php
+++ 06-atelier-my-corp/layout/footer.php
@@ -1,10 +1,10 @@
-<footer class="bg-white dark:bg-gray-900 mt-36">
+<footer class="bg-white dark:bg-gray-900 mt-16">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyCorp</span>
               </a>
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -12,7 +12,7 @@
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                          <a href="https://flowbite.com/" class="hover:underline">MyCorp</a>
                       </li>
                       <li>
                           <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>

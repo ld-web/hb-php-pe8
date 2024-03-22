bdc5e0818f8419accb7bce08a40d103056929005
Lucas Delobelle <lucas@ld-web.net>
2024-03-22 14:08:49 +0100

    :sparkles: Produits : pagination

--- 03-arrays-products/index.php
+++ 03-arrays-products/index.php
@@ -1,6 +1,19 @@
 <?php
 require_once 'data/products.php';
 require_once 'layout/header.php';
+
+const NB_PRODUCTS_PER_PAGE = 10;
+$pageNumber = isset($_GET['page']) ? intval($_GET['page']) : 1;
+if ($pageNumber === 0) {
+    $pageNumber = 1;
+}
+
+$idxStart = NB_PRODUCTS_PER_PAGE * ($pageNumber - 1);
+
+$items = array_slice($products, $idxStart, NB_PRODUCTS_PER_PAGE);
+
+$nbPages = ceil(count($products) / NB_PRODUCTS_PER_PAGE);
+var_dump($nbPages);
 ?>
 
 <section>
@@ -9,9 +22,23 @@ require_once 'layout/header.php';
     </header>
 
     <div class="products-container">
-        <?php foreach ($products as $product) {
+        <?php foreach ($items as $product) {
             require 'templates/product-item.php';
         } ?>
+    </div>
+
+    <div id="pagination">
+        <?php for($i = 1; $i <= $nbPages; $i++) { ?>
+            <span>
+                <?php if ($pageNumber === $i) { ?>
+                    <?php echo $i; ?>
+                <?php } else { ?>
+                    <a href="index.php?page=<?php echo $i; ?>">
+                    <?php echo $i; ?>
+                    </a>
+                <?php } ?>
+            </span>
+        <?php } ?>
     </div>
 </section>
 

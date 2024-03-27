4855f89b73765a373a213cfa877347d96e3c84af
Lucas Delobelle <lucas@ld-web.net>
2024-03-27 10:29:17 +0100

    :sparkles: Produits étoiles pour les notes

--- 03-arrays-products/templates/product-item.php
+++ 03-arrays-products/templates/product-item.php
@@ -7,7 +7,17 @@
             <?php echo $product['name']; ?>
         </h2>
         <div class="product-stars">
-            *****
+        <?php
+        $stars = $product['stars'];
+        for ($i = 0; $i < 5; $i++) {
+            // require __DIR__ . '/star-' . (($i < $stars) ? 'full' : 'empty') . '.php';
+            if ($i < $stars) {
+                require __DIR__ . '/star-full.php';
+            } else {
+                require __DIR__ . '/star-empty.php';
+            }
+        }
+        ?>
         </div>
         <h3>
             <?php echo $product['price']; ?> €
